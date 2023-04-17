2c2c651f4a8414f8aaf3b19caa2693cc6117e660
alex.ai <alexanderemmanuel1719@gmail.com>
2023-04-17 17:05:58 +0000

    adding the allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-fogb=2$#h(zm#fv*a&rih!gx-e2)twfxm1w6%4txx40@!^*$nx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
