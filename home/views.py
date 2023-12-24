26a3bf0c7cb13277e170860f73958412ec5b1a8b
Alex1.ai <alexanderemmanuel1719@gmail.com>
2023-12-24 21:44:22 +0000

    putting the Cname in the config file

--- home/views.py
+++ home/views.py
@@ -13,7 +13,8 @@ def home(request):
 
     products = Product.objects.all().filter(
         is_available=True).order_by('-modified_date')
-
+     
+    reviews=None
     for product in products:
         reviews = ReviewRating.objects.filter(
             product_id=product.id, status=True)
