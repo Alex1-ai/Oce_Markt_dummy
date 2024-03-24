56130e04d6f6c180a18934a461124d408e3f7760
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-03-24 08:53:52 +0000

    removing the blog article

--- home/views.py
+++ home/views.py
@@ -14,7 +14,7 @@ def home(request):
     # print(DEBUG)
     products = Product.objects.all().filter(
         is_available=True).order_by('-modified_date')
-     
+
     reviews=None
     for product in products:
         reviews = ReviewRating.objects.filter(
